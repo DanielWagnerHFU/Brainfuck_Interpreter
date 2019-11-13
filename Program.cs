f5024a4352e160788b13e141b1e521f3d1e938b3
DanielWagnerHFU <45201769+DanielWagnerHFU@users.noreply.github.com>
2019-11-13 14:33:58 +0100

    created project, set terminal to external

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Brainfuck_Interpreter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
